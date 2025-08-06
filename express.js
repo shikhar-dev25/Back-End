70c8bc87ee96dc2c85d83c1a68cf70d3a592f391
shikhar250 <shikhar.maurya2504@gmail.com>
2025-08-06 11:26:08 +0530

    expressjs

--- express.js
+++ express.js
@@ -1,2 +1,11 @@
-const express = require('express')
+const express = require('express');
+const app = express();
 
+app.get('/user/:id', (req, res) => {
+  const userId = req.params.id;
+  res.send(`User ID is: ${userId}`);
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
